e45d9008e0ece68b413d054b1dfb900588a3df57
Cyber-Dragon <luisleon1375@gmail.com>
2016-03-15 08:46:11 -0700

    0.0.1

--- tower.java
+++ tower.java
@@ -6,18 +6,21 @@
  * @version (a version number or a date)
  */
 public class tower {
-  public static void main(String[] args) {
-    int nDisks = 3;
-    doTowers(nDisks, 'A', 'B', 'C');
-  }
+ 
 
-  public static void doTowers(int topN, char from, char inter, char to) {
+  public static void solveTowers(int topN, char from, char inter, char to) {
     if (topN == 1){
       System.out.println("Disk 1 from " + from + " to " + to);
     }else {
-      doTowers(topN - 1, from, to, inter);
+      solveTowers(topN - 1, from, to, inter);
       System.out.println("Disk " + topN + " from " + from + " to " + to);
-      doTowers(topN - 1, inter, from, to);
+      solveTowers(topN - 1, inter, from, to);
     }
+  }
+  
+   public static void main(String[] args) {
+       
+    int nDisks = 5;
+    solveTowers(nDisks, 'A', 'B', 'C');
   }
 }
